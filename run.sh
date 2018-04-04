ab46f0b66985f14036a5f9e07436a5b99bdcf9c3
Jakub Beránek <berykubik@gmail.com>
2018-04-04 10:29:55 +0200

    ENH: optimize OpenMP thread placement

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so
-OMP_CANCELLATION=TRUE OMP_NESTED=TRUE ${DIR}/build/sigmod-2018
+OMP_CANCELLATION=TRUE OMP_NESTED=TRUE OMP_DYNAMIC=TRUE OMP_PLACES=sockets OMP_PROC_BIND=spread ${DIR}/build/sigmod-2018
